0c069d27389e184e4f1c4d7ccbaed66ce2210fd4
Victor Llagas <rava44311@gmail.com>
2022-08-27 18:54:48 -0500

    building structure

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,4 +1,53 @@
-"""
-first commit
+class Parking_garage:
+    def __init__(self, tickets, parkingSpaces, currentTickets=4):
+            self.tickets = tickets
+            self.parkingSpaces = parkingSpaces
+            self.currentTickets = currentTickets
 
-"""
+class Garage:
+    def __init__(self):
+        self.tickets = []
+        self.parkingSpaces = []
+        self.currentTickets = {}
+            
+        
+    def takeTicket(self, tickets, parkingSpaces):
+        print("Checking for space....")
+        if self.parkingSpaces == 0:
+            print("We are currently full!")
+            
+        else:    
+            Parking_garage.tickets -= 1
+            Parking_garage.parkingSpaces -= 1     
+        
+    def payForParking(self):
+        pass
+        """
+        alex's part 
+        
+        """
+
+    def leaveGarage(self, tickets, parkingSpaces):
+        pass
+#         if currentTicket == True:
+#             print("Thank you, have a nice day!")
+#             Parking_garage.tickets += 1
+#             Parking_garage.parkingSpaces += 1
+#             Parking_garage.currentTickets['tickets'] = False
+            
+#         else:
+#             print("Please pay for your ticket before leaving")
+#             self.payForParking()
+
+    def run(self):
+        while True:
+            nav = input("Would you like to enter the garage? (y/n)?")
+            if nav == 'y':
+                self.takeTicket()
+                
+            elif nav == 'n':
+                print("Maybe next time!")
+                break
+
+garage = Garage()
+garage.run()
